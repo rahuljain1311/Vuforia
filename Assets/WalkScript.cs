69e72006bacc95ca1aee74bc9baae9be0e56f12f
Rahul <rahul_jain@mckinsey.com>
2017-06-22 23:36:31 +0530

    Zombie will walk if we press paper on red color

---
+++ Assets/WalkScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Vuforia;
+
+public class WalkScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject button;
+	private GameObject zombie;
+	// Use this for initialization
+	void Start () {
+
+		button = GameObject.Find ("VirtualButton");
+		button.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+		zombie = GameObject.Find ("zombie");
+	}
+
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour vb){
+
+//		Debug.Log ("Button pressed virtual");
+		zombie.GetComponent<Animation>().Play ();
+	}
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb){
+
+		zombie.GetComponent<Animation>().Stop();
+//		Debug.Log ("Button released virtual");
+	}
+}
